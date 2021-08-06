bb7e577e9085e80d10f67c75bd12fc090ec6165e
tubuanha <tubuanha@users.noreply.github.com>
2021-08-06 04:37:58 +0000

    Lint

--- index.js
+++ index.js
@@ -38,7 +38,8 @@ cron.schedule('0,15 * * * *', () => {
 client
   .once('ready', () => {
     console.log(`${client.user.tag} でログインしました。`)
-  }).on('messageCreate', async (message) => {
+  })
+  .on('messageCreate', async (message) => {
     if (message.content.startsWith(prefix)) {
       const _spl = message.content.trim().split(' ')
       const command = _spl[0].slice(1)
