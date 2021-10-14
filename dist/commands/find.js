9e82130b485734c4548f19340ead6cfc658eb992
tubuanha <tubuanha@users.noreply.github.com>
2021-10-14 04:01:42 +0000

    Build

--- dist/commands/find.js
+++ dist/commands/find.js
@@ -1,46 +1,78 @@
-"use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+'use strict';
+var __awaiter =
+  (this && this.__awaiter) ||
+  function (thisArg, _arguments, P, generator) {
+    function adopt(value) {
+      return value instanceof P
+        ? value
+        : new P(function (resolve) {
+            resolve(value);
+          });
+    }
     return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
+      function fulfilled(value) {
+        try {
+          step(generator.next(value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function rejected(value) {
+        try {
+          step(generator['throw'](value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function step(result) {
+        result.done
+          ? resolve(result.value)
+          : adopt(result.value).then(fulfilled, rejected);
+      }
+      step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const builders_1 = require("@discordjs/builders");
-const discord_js_1 = require("discord.js");
-const Bot_1 = __importDefault(require("../Components/Bot"));
+  };
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
+Object.defineProperty(exports, '__esModule', { value: true });
+const builders_1 = require('@discordjs/builders');
+const discord_js_1 = require('discord.js');
+const Bot_1 = __importDefault(require('../Components/Bot'));
 module.exports = {
-    data: new builders_1.SlashCommandBuilder()
-        .setName('find')
-        .setDescription('スレッドをタイトルから検索します。')
-        .addStringOption((option) => option
+  data: new builders_1.SlashCommandBuilder()
+    .setName('find')
+    .setDescription('スレッドをタイトルから検索します。')
+    .addStringOption((option) =>
+      option
         .setName('query')
         .setDescription('検索キーワードを入力してください。')
-        .setRequired(true)),
-    execute(interaction) {
-        return __awaiter(this, void 0, void 0, function* () {
-            yield interaction.deferReply();
-            const query = interaction.options.getString('query');
-            const results = [];
-            Bot_1.default.db.query('SELECT * FROM `threads` WHERE `title` LIKE ?', [`%${query}%`], (e, rows) => {
-                for (const row of rows) {
-                    results.push(`[${row.title}](${row.firstMessageUrl})`);
-                }
-                interaction.editReply({
-                    embeds: [
-                        new discord_js_1.MessageEmbed()
-                            .setTitle(`「${query}」の検索結果`)
-                            .setDescription(results.join('\n\n'))
-                            .setColor('BLURPLE'),
-                    ],
-                });
-            });
-        });
-    },
+        .setRequired(true)
+    ),
+  execute(interaction) {
+    return __awaiter(this, void 0, void 0, function* () {
+      yield interaction.deferReply();
+      const query = interaction.options.getString('query');
+      const results = [];
+      Bot_1.default.db.query(
+        'SELECT * FROM `threads` WHERE `title` LIKE ?',
+        [`%${query}%`],
+        (e, rows) => {
+          for (const row of rows) {
+            results.push(`[${row.title}](${row.firstMessageUrl})`);
+          }
+          interaction.editReply({
+            embeds: [
+              new discord_js_1.MessageEmbed()
+                .setTitle(`「${query}」の検索結果`)
+                .setDescription(results.join('\n\n'))
+                .setColor('BLURPLE'),
+            ],
+          });
+        }
+      );
+    });
+  },
 };
--- dist/regist.js
+++ dist/regist.js
@@ -1,38 +1,82 @@
-"use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
+'use strict';
+var __createBinding =
+  (this && this.__createBinding) ||
+  (Object.create
+    ? function (o, m, k, k2) {
+        if (k2 === undefined) k2 = k;
+        Object.defineProperty(o, k2, {
+          enumerable: true,
+          get: function () {
+            return m[k];
+          },
+        });
+      }
+    : function (o, m, k, k2) {
+        if (k2 === undefined) k2 = k;
+        o[k2] = m[k];
+      });
+var __setModuleDefault =
+  (this && this.__setModuleDefault) ||
+  (Object.create
+    ? function (o, v) {
+        Object.defineProperty(o, 'default', { enumerable: true, value: v });
+      }
+    : function (o, v) {
+        o['default'] = v;
+      });
+var __importStar =
+  (this && this.__importStar) ||
+  function (mod) {
     if (mod && mod.__esModule) return mod;
     var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    if (mod != null)
+      for (var k in mod)
+        if (k !== 'default' && Object.prototype.hasOwnProperty.call(mod, k))
+          __createBinding(result, mod, k);
     __setModuleDefault(result, mod);
     return result;
-};
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+  };
+var __awaiter =
+  (this && this.__awaiter) ||
+  function (thisArg, _arguments, P, generator) {
+    function adopt(value) {
+      return value instanceof P
+        ? value
+        : new P(function (resolve) {
+            resolve(value);
+          });
+    }
     return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
+      function fulfilled(value) {
+        try {
+          step(generator.next(value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function rejected(value) {
+        try {
+          step(generator['throw'](value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function step(result) {
+        result.done
+          ? resolve(result.value)
+          : adopt(result.value).then(fulfilled, rejected);
+      }
+      step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const path_1 = __importDefault(require("path"));
-const dotenv = __importStar(require("dotenv"));
+  };
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
+Object.defineProperty(exports, '__esModule', { value: true });
+const path_1 = __importDefault(require('path'));
+const dotenv = __importStar(require('dotenv'));
 dotenv.config();
 const fs = require('fs');
 const { REST } = require('@discordjs/rest');
@@ -40,30 +84,39 @@ const { Routes } = require('discord-api-types/v9');
 const clientId = '897477971512819713';
 const commands = [];
 const commandFiles = fs
-    .readdirSync(path_1.default.resolve(__dirname, 'Commands'))
-    .filter((file) => file.endsWith('.js'));
+  .readdirSync(path_1.default.resolve(__dirname, 'Commands'))
+  .filter((file) => file.endsWith('.js'));
 for (const file of commandFiles) {
-    const command = require(`./Commands/${file}`);
-    commands.push(command.data.toJSON());
+  const command = require(`./Commands/${file}`);
+  commands.push(command.data.toJSON());
 }
 const rest = new REST({ version: '9' }).setToken(process.env.DISCORD_TOKEN);
-(() => __awaiter(void 0, void 0, void 0, function* () {
+(() =>
+  __awaiter(void 0, void 0, void 0, function* () {
     try {
-        // テスト鯖
-        yield rest.put(Routes.applicationGuildCommands(clientId, '747053403154284605'), {
-            body: commands,
-        });
-        // アプリにゃんこ大戦争
-        yield rest.put(Routes.applicationGuildCommands(clientId, '755774191613247568'), {
-            body: commands,
-        });
-        // アプリにゃんこ大戦争(サブ)
-        yield rest.put(Routes.applicationGuildCommands(clientId, '796606104410783784'), {
-            body: commands,
-        });
-        console.log('コマンドを登録しました。');
+      // テスト鯖
+      yield rest.put(
+        Routes.applicationGuildCommands(clientId, '747053403154284605'),
+        {
+          body: commands,
+        }
+      );
+      // アプリにゃんこ大戦争
+      yield rest.put(
+        Routes.applicationGuildCommands(clientId, '755774191613247568'),
+        {
+          body: commands,
+        }
+      );
+      // アプリにゃんこ大戦争(サブ)
+      yield rest.put(
+        Routes.applicationGuildCommands(clientId, '796606104410783784'),
+        {
+          body: commands,
+        }
+      );
+      console.log('コマンドを登録しました。');
+    } catch (error) {
+      console.error(error);
     }
-    catch (error) {
-        console.error(error);
-    }
-}))();
+  }))();
