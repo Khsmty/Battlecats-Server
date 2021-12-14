d4e2aa5ca15924700c64c857b20e95b38365fb88
tubuanha <mail@taigasaito.rsvr.jp>
2021-12-14 13:22:27 +0900

    add: setActivity()

--- src/Events/ready.ts
+++ src/Events/ready.ts
@@ -5,5 +5,6 @@ module.exports = {
   once: true,
   execute(client: Client) {
     console.log(`Ready! Logged in as ${client.user?.tag}`);
+    client.user?.setActivity('にゃんこ大戦争', { type: 'COMPETING' });
   },
 };
