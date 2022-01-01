4776826ea9dbee040b270e784ada8528a8ee7a00
Tubuanha <mail@taigasaito.com>
2022-01-02 00:37:25 +0900

    Fix: botが落ちる問題

--- src/Components/NgImage.ts
+++ src/Components/NgImage.ts
@@ -28,82 +28,82 @@ export default function (message: Message) {
     for (const attachment of attachments) {
       if (!attachment.height && !attachment.width) continue;
 
-      const img = await axios.get(attachment.url, { responseType: 'arraybuffer' });
-      const imgBase64 = Buffer.from(img.data, 'binary').toString('base64');
+      try {
+        const img = await axios.get(attachment.url, { responseType: 'arraybuffer' });
+        const imgBase64 = Buffer.from(img.data, 'binary').toString('base64');
 
-      const ocr: any = await axios
-        .post('https://ocr-example.herokuapp.com/base64', {
+        const ocr: any = await axios.post('https://ocr-example.herokuapp.com/base64', {
           base64: imgBase64,
           languages: 'eng,jpn',
-        })
-        .catch(() => {
-          return;
         });
-      const text = ocr.data.result;
+        const text = ocr.data.result;
 
-      let ngWord: any = false;
-      for (const row of rows) {
-        if (text.toLowerCase().replace(/ |\n/g, '').includes(row.word)) {
-          ngWord = row;
+        let ngWord: any = false;
+        for (const row of rows) {
+          if (text.toLowerCase().replace(/ |\n/g, '').includes(row.word)) {
+            ngWord = row;
+          }
         }
-      }
 
-      if (!ngWord) continue;
+        if (!ngWord) continue;
 
-      const imageLogChannel: AnyChannel | undefined =
-        message.client.channels.cache.get('904889227085508658');
+        const imageLogChannel: AnyChannel | undefined =
+          message.client.channels.cache.get('904889227085508658');
 
-      if (!imageLogChannel || !imageLogChannel.isText()) return;
+        if (!imageLogChannel || !imageLogChannel.isText()) return;
 
-      const sendMsg = await imageLogChannel.send({
-        files: [
-          {
-            attachment: attachment.url,
-            name: attachment.url.split('/').pop(),
-          },
-        ],
-      });
+        const sendMsg = await imageLogChannel.send({
+          files: [
+            {
+              attachment: attachment.url,
+              name: attachment.url.split('/').pop(),
+            },
+          ],
+        });
 
-      const embed: MessageEmbed = new MessageEmbed()
-        .setAuthor({ name: message.author.tag, iconURL: message.author.displayAvatarURL() })
-        .setDescription(text)
-        .setImage(String(sendMsg.attachments.first()?.url))
-        .setFooter(`#${msgChannel.name}`);
+        const embed: MessageEmbed = new MessageEmbed()
+          .setAuthor({ name: message.author.tag, iconURL: message.author.displayAvatarURL() })
+          .setDescription(text)
+          .setImage(String(sendMsg.attachments.first()?.url))
+          .setFooter(`#${msgChannel.name}`);
 
-      if (ngWord.delmsg) {
-        message.delete().catch(() => {});
+        if (ngWord.delmsg) {
+          message.delete().catch(() => {});
 
-        const authorEmbed: MessageEmbed = new MessageEmbed()
-          .setColor('RED')
-          .setDescription(
-            `画像内に NGワード「||${ngWord.word}||」が含まれていたため、削除しました。`
-          );
+          const authorEmbed: MessageEmbed = new MessageEmbed()
+            .setColor('RED')
+            .setDescription(
+              `画像内に NGワード「||${ngWord.word}||」が含まれていたため、削除しました。`
+            );
 
-        message.author
-          .send({
-            embeds: [authorEmbed],
-          })
-          .catch(() => {
-            message.channel.send({
-              content: `<@!${message.author.id}>`,
+          message.author
+            .send({
               embeds: [authorEmbed],
+            })
+            .catch(() => {
+              message.channel.send({
+                content: `<@!${message.author.id}>`,
+                embeds: [authorEmbed],
+              });
             });
-          });
 
-        embed.setColor('RED').setTitle('NG画像削除');
-      } else {
-        embed.setColor('YELLOW').setTitle('NG画像検出').setURL(message.url);
+          embed.setColor('RED').setTitle('NG画像削除');
+        } else {
+          embed.setColor('YELLOW').setTitle('NG画像検出').setURL(message.url);
+        }
+
+        const ngLogChannel: AnyChannel | undefined = message.client.channels.cache.get(
+          config.channels.ngLog
+        );
+
+        if (!ngLogChannel || !ngLogChannel.isText()) return;
+
+        ngLogChannel.send({
+          embeds: [embed],
+        });
+      } catch (e) {
+        continue;
       }
-
-      const ngLogChannel: AnyChannel | undefined = message.client.channels.cache.get(
-        config.channels.ngLog
-      );
-
-      if (!ngLogChannel || !ngLogChannel.isText()) return;
-
-      ngLogChannel.send({
-        embeds: [embed],
-      });
     }
   });
 }
