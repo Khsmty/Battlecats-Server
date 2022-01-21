b2e03bb6d5a89abeb66fb2c7fae0239205601fd2
Tubuanha <mail@taigasaito.org>
2022-01-21 16:47:03 +0900

    Add: reboot command

---
+++ src/MessageCommands/reboot.ts
@@ -0,0 +1,27 @@
+import { Message } from 'discord.js';
+import { execSync } from 'child_process';
+import config from '../config.json';
+
+module.exports = {
+  name: 'reboot',
+  async execute(message: Message, args: string[]) {
+    if (message.author.id !== config.ownerId) return;
+
+    if (args[0] === 'update') {
+      await message.channel.send('Updating...');
+
+      let buf = execSync('git pull');
+
+      await message.channel.send('Result:\n```\n' + buf.toString() + '\n```');
+      await message.channel.send('Compiling...');
+
+      buf = execSync('npm run build');
+
+      await message.channel.send('Result:\n```\n' + buf.toString() + '\n```');
+    }
+
+    await message.channel.send('Rebooting...');
+
+    process.exit(0);
+  },
+};
