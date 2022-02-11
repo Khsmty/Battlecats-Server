f2fafe9325266174a4b73d64f2819ef7076a5088
Tubuanha / つぶあん派 <70151015+tubuanha@users.noreply.github.com>
2022-02-12 00:08:00 +0900

    Change: モデレーターメンション2

--- src/Components/NgImage.ts
+++ src/Components/NgImage.ts
@@ -118,6 +118,7 @@ export default function (message: Message) {
         if (!ngLogChannel || !ngLogChannel.isText()) return;
 
         ngLogChannel.send({
+          content: '<@&903921596241182731>',
           embeds: [embed],
         });
       } catch (e) {
