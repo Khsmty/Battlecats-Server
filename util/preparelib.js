bc1e993c435f2cc6486b307d2549e04b1ba48c42
tubuanha <tubuanha@users.noreply.github.com>
2022-03-01 09:41:05 +0000

    Format

--- util/preparelib.js
+++ util/preparelib.js
@@ -1,12 +1,12 @@
-const { execFileSync } = require("child_process");
-const path = require("path");
+const { execFileSync } = require('child_process');
+const path = require('path');
 
-const dirs = __dirname.replace(/\\/g, "/").split("/");
-if(dirs[dirs.length - 3] === "node_modules"){
-  console.log("Preparing library...");
-  let commandLine = ["npx", "tsc", "-p", path.join(__dirname, "../tsconfig.lib.json")];
-  if(process.platform === "win32"){
-    commandLine = ["cmd", "/c", ...commandLine];
+const dirs = __dirname.replace(/\\/g, '/').split('/');
+if (dirs[dirs.length - 3] === 'node_modules') {
+  console.log('Preparing library...');
+  let commandLine = ['npx', 'tsc', '-p', path.join(__dirname, '../tsconfig.lib.json')];
+  if (process.platform === 'win32') {
+    commandLine = ['cmd', '/c', ...commandLine];
   }
   const executable = commandLine.shift();
   execFileSync(executable, commandLine);
