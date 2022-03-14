1fa49d289a19c8eb24b6cea3ec498068a950eb88
Tubuanha <70151015+tubuanha@users.noreply.github.com>
2022-03-14 21:22:11 +0900

    fix: bump 通知が動かない (暫定)

--- src/Events/messageCreate.ts
+++ src/Events/messageCreate.ts
@@ -13,8 +13,8 @@ module.exports = {
     if (message.channelId === config.upChannel) {
       // DISBOARD
       if (
-        message.author.id === '302050872383242240' &&
-        message.embeds[0].description?.match(/表示順をアップしたよ/)
+        message.author.id === '302050872383242240' // &&
+        // message.embeds[0].description?.match(/表示順をアップしたよ/)
       ) {
         UpdateBoard(message);
       }
