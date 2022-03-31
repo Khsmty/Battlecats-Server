6c5ad9e45735c5c04ee832edc91a47d7a10327fe
Tubuanha <70151015+tubuanha@users.noreply.github.com>
2022-04-01 00:13:52 +0900

    feat: april fool

--- src/Events/ready.ts
+++ src/Events/ready.ts
@@ -5,10 +5,9 @@ module.exports = {
   once: true,
   execute(client: Client) {
     console.log('Bot started.');
-    client.user?.setActivity('にゃんこ大戦争', { type: 'COMPETING' });
 
     setInterval(() => {
-      client.user?.setActivity('にゃんこ大戦争', { type: 'COMPETING' });
+      client.user?.setActivity('わんこ大戦争', { type: 'COMPETING' });
     }, 1000 * 60 * 60);
   },
 };
